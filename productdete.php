348c1e660b471d51244dd4e05a397971d277a9c9
FreeMovie2 <67011329+FreeMovie2@users.noreply.github.com>
2020-06-18 22:23:03 +0700

    Add files via upload

--- productdete.php
+++ productdete.php
@@ -1,33 +1,37 @@
-<?php 
+<?php
 
-  
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=320, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+    <meta name="viewport" content="width=320, initial-scale=1, maximum-scale=1, user-scalable=0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Thaiderm Marble</title>
     <script src='https://kit.fontawesome.com/a076d05399.js'></script>
 
-<!-- Section Meta tag -->
+    <!-- Section Meta tag -->
     <?php include_once('includes/meta.php') ?>
 
-<!-- CSS -->
+    <!-- CSS -->
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="node_modules/font-awesome/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Prompt" rel="stylesheet">
+    <link rel="stylesheet" href="node_modules/owl.carousel/dist/assets/owl.carousel.min.css" />
+    <link rel="stylesheet" href="node_modules/owl.carousel/dist/assets/owl.theme.default.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
-    
- 
+
+
 </head>
+
 <body>
-    
-<!-- Section Navbar -->
+
+    <!-- Section Navbar -->
     <?php include_once('includes/navbar.php') ?>
 
-<!-- Section Page-title -->
+    <!-- Section Page-title -->
     <header class="jarallax" data-speed="0.5" style="background-image: url(assets/images/03.JPG);">
         <div class="page-image">
             <h1 class="display-4 font-weight-bold">รายละเอียดสินค้า</h1>
@@ -35,64 +39,114 @@
         </div>
     </header>
     <section class="jumbotron-fluid">
-    <h1 class="border-short-bottom text-center">รายละเอียดสินค้า</h1>
-<div class="container">
-  <div class="jumbotron row">
-    <div class="col-12 col-sm-7 col-md-6 p-3">
-    <img src="assets/images/02.JPG" class="img-fluid border border-danger" alt="Responsive image">
-    </div>
-    <div class="col-12 col-sm-6 col-md-4 p-3">
-    <h3 class="font-weight-bold">ชื่อสินค้า</h3>
-    <h5 class="font-weight-light text-muted">รหัสสินค้า</h5><br>
-    <h4 class="font-weight-normal text-danger">ราคา</h4><br>
-    <h4 class="font-weight-normal ">รายละเอียด</h4>
-    <p class="font-weight-normal ">ราคาสีแดงเป็นราคาเริ่มต้นของการซื้อยก SLAB เท่านั้น ราคาอาจแตกต่างกันขึ้นอยู่กับสีและลวดลายของหิน</p><br>
-    <i class="fa fa-share-alt" style="font-size:24px;color:red"> แชร์</i>
-    <i class="fa fa-facebook-official" style="font-size:24px;color:blue"></i>
-    <i class="fab fa-line" style="font-size:24px;color:green"></i>
-    </div>
-  </div>
-
-  <div class="row justify-content-between">
-    <div class="col-12 col-sm-6 col-md-6 p-3">
-    <div class="text-center">
-    <i class="fas fa-award" style="font-size:100px"></i>
-    </div>
-    <h4 class="font-weight-bold text-center">การรับประกันสินค้า</h4>
-    <p class="font-weight-normal float-left">- บริษัทฯ มีการรับประกันสินค้า ขึ้นอยู่กับประเภทของสินค้า โดยจะอยู่ภายใต้การพิจราณาของบริษัท</p>
-    <p class="font-weight-normal float-left">- ประกันสินค้าแตกหักเสียหายขณะขนส่ง และหลังจากขนส่งภายใน 7 วันเท่านั้น</p>
-    <p class="font-weight-normal float-left">- การรับประกินสินค้าของแต่ละรายการมีความแตกต่างกัน</p>
-    <p class="font-weight-normal float-left">หมายเหตุ : บริษัทฯ ขอสงวนสิทธิ์ในการพิจารณาตรวจสอบก่อนจัดส่งสินค้าใหม่ให้แก่ลูกค้า</p>
-    </div>
-    
-    <div class="col-12 col-sm-6 col-md-6 p-3">
-    <div class="text-center">
-    <i class="fas fa-info-circle" style="font-size:100px"></i>
-    </div>
-    <h4 class="font-weight-bold text-center">หมายเหตุ</h4>
-    <p class="font-weight-normal float-left">- ราคาข้างต้นไม่รวมภาษีมูลค่าเพิ่ม</p>
-    <p class="font-weight-normal float-left">- ราคาอาจเปลี่ยนแปลงโดยไม่ต้องแจ้งให้ทราบล่วงหน้า</p>
-    </div>
-  </div>
-  
-</div>
+        <h1 class="border-short-bottom text-center">รายละเอียดสินค้า</h1>
+        <div class="container">
+            <div class="jumbotron row">
+                <div class="col-12 col-sm-7 col-md-6 p-3">
+                    <img src="assets/images/02.JPG" class="img-fluid border border-danger" alt="Responsive image">
+                </div>
+                <div class="col-12 col-sm-6 col-md-4 p-3">
+                    <h3 class="font-weight-bold">ชื่อสินค้า</h3>
+                    <h5 class="font-weight-light text-muted">รหัสสินค้า</h5><br>
+                    <h4 class="font-weight-normal text-danger">ราคา</h4><br>
+                    <h4 class="font-weight-normal ">รายละเอียด</h4>
+                    <p class="font-weight-normal ">ราคาสีแดงเป็นราคาเริ่มต้นของการซื้อยก SLAB เท่านั้น ราคาอาจแตกต่างกันขึ้นอยู่กับสีและลวดลายของหิน</p><br>
+                    <i class="fa fa-share-alt" style="font-size:24px;color:red"> แชร์</i>
+                    <i class="fa fa-facebook-official" style="font-size:24px;color:blue"></i>
+                    <i class="fab fa-line" style="font-size:24px;color:green"></i>
+                </div>
+            </div>
+            <div class="row justify-content-between">
+                <div class="col-12 col-sm-6 col-md-6 p-3">
+                    <div class="text-center">
+                        <i class="fas fa-award" style="font-size:100px"></i>
+                    </div>
+                    <h4 class="font-weight-bold text-center">การรับประกันสินค้า</h4>
+                    <p class="font-weight-normal float-left">- บริษัทฯ มีการรับประกันสินค้า ขึ้นอยู่กับประเภทของสินค้า โดยจะอยู่ภายใต้การพิจราณาของบริษัท</p>
+                    <p class="font-weight-normal float-left">- ประกันสินค้าแตกหักเสียหายขณะขนส่ง และหลังจากขนส่งภายใน 7 วันเท่านั้น</p>
+                    <p class="font-weight-normal float-left">- การรับประกินสินค้าของแต่ละรายการมีความแตกต่างกัน</p>
+                    <p class="font-weight-normal float-left">หมายเหตุ : บริษัทฯ ขอสงวนสิทธิ์ในการพิจารณาตรวจสอบก่อนจัดส่งสินค้าใหม่ให้แก่ลูกค้า</p>
+                </div>
+                <div class="col-12 col-sm-6 col-md-6 p-3">
+                    <div class="text-center">
+                        <i class="fas fa-info-circle" style="font-size:100px"></i>
+                    </div>
+                    <h4 class="font-weight-bold text-center">หมายเหตุ</h4>
+                    <p class="font-weight-normal float-left">- ราคาข้างต้นไม่รวมภาษีมูลค่าเพิ่ม</p>
+                    <p class="font-weight-normal float-left">- ราคาอาจเปลี่ยนแปลงโดยไม่ต้องแจ้งให้ทราบล่วงหน้า</p>
+                </div>
+            </div>
+        </div>
+        <section class="container jumbotron">
+            <h1 class="border-short-bottom text-center">สินค้ายอดนิยม</h1>
+            <div class="col-12">
+                <div class="owl-carousel owl-theme">
+                        <div class="card" style="width: 18rem;">
+                            <img src="assets/images/02.JPG" class="card-img-top" alt="...">
+                            <div class="card-body text-center">
+                                <h5 class="card-title">ชื่อสินค้า</h5>
+                                <p class="card-text">ราคา</p>
+                            </div>
+                        </div>
+                </div>
+        </section>
     </section>
 
-<!-- Section Footer -->
+    <!-- Section Footer -->
     <?php include_once('includes/footer.php') ?>
-    <!-- <?php include_once('php/userlogs.php') ?> -->
+
 
     <div class="to-top">
         <i class="fa fa-angle-up" aria-hidden="true"></i>
     </div>
-        
+
+
 
     <script src="node_modules/jquery/dist/jquery.min.js"></script>
     <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
     <script src="node_modules/jarallax/dist/jarallax.min.js"></script>
+    <script src="node_modules/owl.carousel/dist/owl.carousel.min.js"></script>
     <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAN7pVYXyLuKkftPkDMFhpTjov4MYVxTnY&callback=initMap"></script>
     <script src="assets/js/main.js"></script>
 
+    <script>
+        $(document).ready(function() {
+            $('.owl-carousel').owlCarousel({
+                loop: true,
+                nav: false,
+                dots: true,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    600: {
+                        items: 2
+                    },
+                    1000: {
+                        items: 3
+                    }
+                }
+            });
+        });
+
+        //              DO NOT IMPLEMENT                //
+        //       this code through the following        //
+        //                                              //
+        //   Floodlight Pixel Manager                   //
+        //   DCM Pixel Manager                          //
+        //   Any system that places code in an iframe   //
+        (function() {
+            var s = document.createElement('script');
+            s.type = 'text/javascript';
+            s.async = true;
+            s.src = ('https:' == document.location.protocol ? 'https://s' : 'http://i') +
+                '.po.st/static/v4/post-widget.js#publisherKey=556iuqt4139l475oo1e8';
+            var x = document.getElementsByTagName('script')[0];
+            x.parentNode.insertBefore(s, x);
+        })();
+    </script>
 </body>
+
+
 </html>
